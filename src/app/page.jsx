b2a11feaa8ea972e8006852fd51c57836a3eb32a
Anjan Karmaker <77603380+anjankarmakar10@users.noreply.github.com>
2024-02-14 11:14:01 +0600

    Build container

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
+import Container from "@/components/Container/Container";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <Container>Hello</Container>
+    </main>
+  );
 }
