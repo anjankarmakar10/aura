e96b1187d4baa21dffb064386cdf5b0006cdc0f2
Anjan Karmaker <77603380+anjankarmakar10@users.noreply.github.com>
2024-02-14 10:34:59 +0600

    build demo products data

---
+++ src/data/products.js
@@ -0,0 +1,51 @@
+const products = [
+  {
+    id: crypto.randomUUID(),
+    name: "Moisturising Cream",
+    info: "CeraVe Moisturising Cream For Dry To Very Dry Skin",
+    price: "18.00",
+    image: "/public/product8.webp",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "odacite",
+    info: "A Winter In Paris",
+    price: "48.00",
+    image: "/public/product2.webp",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "goop",
+    info: "g. tox malachite + Fruit Acid Por purifying Cleanser",
+    price: "48.00",
+    image: "/public/product3.webp",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "leahlani",
+    info: "Citrus & Citrine Regenerating Toning Mist",
+    price: "48.00",
+    image: "/public/product4.webp",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "acure",
+    info: "Ultra Hydrating Cleanser",
+    price: "48.00",
+    image: "/public/product5.webp",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Kora organics",
+    info: "Noni Glow Sleeping Mask",
+    price: "48.00",
+    image: "/public/product6.webp",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Kora organics",
+    info: "Noni Glow Sleeping Mask",
+    price: "48.00",
+    image: "/public/product7.webp",
+  },
+];
