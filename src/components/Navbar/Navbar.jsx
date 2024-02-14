2021f96b27d8b24355e4fe762fded599c0cb2b7c
Anjan Karmaker <77603380+anjankarmakar10@users.noreply.github.com>
2024-02-14 20:26:06 +0600

    Implement navbar background color

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,31 @@
+"use client";
+
 import Logo from "../Logo/Logo";
 import MobileNavbar from "../MobileNavbar/MobileNavbar";
 import styles from "./Navbar.module.css";
 import Button from "../Button/Button";
 import Navlinks from "../Navlinks/Navlinks";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 const Navbar = () => {
+  const [isDown, setIsDown] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      if (window.scrollY > 50) {
+        setIsDown(true);
+      } else {
+        setIsDown(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [isDown]);
+
   return (
-    <div>
+    <div className={clsx(styles.bg, isDown ? styles.bgShow : styles.bgHide)}>
       <nav className={styles.navbar}>
         <div className={styles.navStart}>
           <Logo />
