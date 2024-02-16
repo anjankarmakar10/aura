bc7ac1673492b8e7a2be8f3eb2456ef5ec04e2d0
Anjan Karmaker <77603380+anjankarmakar10@users.noreply.github.com>
2024-02-16 12:51:53 +0600

    set favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo-white.svg" sizes="any" />
+      </head>
       <body className={inter.className}>
         <Container>
           <Navbar />
